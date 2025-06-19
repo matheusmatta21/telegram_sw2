a413da6f05cc03b1716f096121d3b748e2732b66
matheusmatta21 <matheusalexandre2004@gmail.com>
2025-06-19 14:57:57 -0300

    feat: add gerar_sessao.py

---
+++ telegram_app/gerar_sessao.py
@@ -0,0 +1,13 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+
+with TelegramClient(StringSession(), api_id, api_hash) as client:
+    print("⚠️ Copie e salve sua StringSession abaixo:")
+    print(client.session.save())
